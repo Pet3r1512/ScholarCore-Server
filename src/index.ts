43e4b0e850047fc8e2eaf4d74b00d9883dd0e064
lekhoi2709 <ledinhkhoimd@gmail.com>
2024-09-18 08:35:17 +0700

    ops: setup Prisma and Xata

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from "hono";
+import prisma from "./db";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Welcome to ScholarCore Server!");
+app.get("/", async (c) => {
+  const count = await prisma.user.count();
+  return c.text(`Welcome to ScholarCore Server! ${count}`);
 });
 
 export default {
